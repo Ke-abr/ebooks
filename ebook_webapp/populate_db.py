6ff66f01314cd2352d2fa859f3ede98fd23a067e
Ke-abr <abreu.kevin91@gmail.com>
2024-09-10 15:32:08 +0200

    get author and title from file name and not metadata

--- ebook_webapp/populate_db.py
+++ ebook_webapp/populate_db.py
@@ -4,8 +4,9 @@ from ebooklib.epub import EpubException
 from zipfile import BadZipFile
 from app import app
 import os
+import re
 
-EBOOK_DIR = '/mnt/h/DL/ebookCollection/ebookCollection'
+EBOOK_DIR = '/mnt/h/DL/testCollection'
 
 COVER_DIR = os.path.join('static', 'covers')
 os.makedirs(COVER_DIR, exist_ok=True)
@@ -13,12 +14,11 @@ os.makedirs(COVER_DIR, exist_ok=True)
 def extract_metadata(epub_path):
     try:
         book = epub.read_epub(epub_path)
-        title = book.get_metadata('DC', 'title')[0][0] if book.get_metadata('DC', 'title') else 'Unknown Title'
-        if not title:
-            title = "Unknown Title"
-        author = book.get_metadata('DC', 'creator')[0][0] if book.get_metadata('DC', 'creator') else 'Unknown Author'
-        if not author:
-            author = "Unknown Author"
+        base_name = os.path.basename(epub_path).replace('.epub', '')
+        match = re.match(r'(.*) - (.*)', base_name)
+        if match:
+            title = match.group(1)
+            author = match.group(2)
         description = book.get_metadata('DC', 'description')[0][0] if book.get_metadata('DC', 'description') else "No Description"
         if not description:
             description = "Unknwon Description"
